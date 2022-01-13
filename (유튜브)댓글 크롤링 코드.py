2a47af8980b7a1975c0d93b0a1b4eac55b871138
minanalysis <94737570+minanalysis@users.noreply.github.com>
2022-01-13 14:47:45 +0900

    Create (유튜브)댓글 크롤링 코드.py

---
+++ (유튜브)댓글 크롤링 코드.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from googleapiclient.discovery import build
+
+
+comments = list()
+api_key = 'AIzaSyDMirfFkPzFdY8wpmLcbBxFHdyY3eXDKHk'
+video_id = 'V3ZR8bBptS4'
+api_obj = build('youtube', 'v3', developerKey=api_key)
+response = api_obj.commentThreads().list(part='snippet,replies', videoId=video_id, maxResults=100).execute()
+
+while response:
+    for item in response['items']:
+        comment = item['snippet']['topLevelComment']['snippet']
+        comments.append([comment['textDisplay'], comment['authorDisplayName'], comment['publishedAt'], comment['likeCount']])
+ 
+        if item['snippet']['totalReplyCount'] > 0:
+            for reply_item in item['replies']['comments']:
+                reply = reply_item['snippet']
+                comments.append([reply['textDisplay'], reply['authorDisplayName'], reply['publishedAt'], reply['likeCount']])
+ 
+    if 'nextPageToken' in response:
+        response = api_obj.commentThreads().list(part='snippet,replies', videoId=video_id, pageToken=response['nextPageToken'], maxResults=100).execute()
+    else:
+        break
+
+df = pd.DataFrame(comments)
+# 엑셀(또는 csv로) 저장
+df.to_csv('results.csv', header=['comment', 'author', 'date', 'num_likes'], index=None)
