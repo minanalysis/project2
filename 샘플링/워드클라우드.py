35a305afd0c8d074d44a2971c969ee3be281d15f
minanalysis <94737570+minanalysis@users.noreply.github.com>
2022-03-05 18:15:37 +0900

    Update 워드클라우드.py

--- 샘플링/워드클라우드.py
+++ 샘플링/워드클라우드.py
@@ -4,10 +4,10 @@ plt.rcParams['font.size'] = 15                    # matplotlib의 글꼴 크기
 plt.rcParams['font.family'] = 'NanumGothicCoding' # matplotlib의 글꼴
 
 wordcloud = WordCloud(
-    font_path='C:\\Users\\JaeIn\\AppData\\Local\\Microsoft\\Windows\\Fonts\\NanumGothicCoding.ttf',
+    font_path='폰트경로',
     background_color='white',
     relative_scaling=0.7
-).generate(' '.join(copy['댓글']))
+).generate(df['댓글']))
 
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis('off')
